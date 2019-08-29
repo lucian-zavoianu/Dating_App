6e91a720ea3bdb0c3812bd29796750a1d9de76a9
Lucian Zavoianu <lucian.zavoianu@oracle.com>
2019-08-29 14:13:46 +0200

    Added Pagination

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { User } from '../../_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from '../../_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -11,13 +12,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+    // console.log(this.pagination.currentPage);
+  }
+
+  loadUsers() {
+    this.userService
+      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe(
+        (response: PaginatedResult<User[]>) => {
+          this.users = response.result;
+          this.pagination = response.pagination;
+        },
+        error => {
+          this.alertify.error(error);
+        });
   }
 
   // ================================ //
