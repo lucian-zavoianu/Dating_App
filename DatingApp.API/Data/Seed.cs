78ee92eb7ecf5865f776bb682de37c172d88cde2
Lucian Zavoianu <lucian.zavoianu@oracle.com>
2019-10-10 14:00:23 +0200

    Updated the Seed Method with Roles

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -9,16 +9,50 @@ namespace DatingApp.API.Data
 {
     public class Seed
     {
-        public static void SeedUsers(UserManager<User> userManager)
+        public static void SeedUsers(UserManager<User> userManager, RoleManager<Role> roleManager)
         {
             if (!userManager.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
 
+                // Create Roles
+                var roles = new List<Role>
+                {
+                    new Role {
+                        Name = "Admin"
+                    },
+                    new Role {
+                        Name = "Moderator"
+                    },
+                    new Role {
+                        Name = "Member"
+                    }
+                };
+
+                foreach (var role in roles)
+                {
+                    roleManager.CreateAsync(role).Wait();
+                }
+
                 foreach (var user in users)
                 {
                     userManager.CreateAsync(user, "password").Wait();
+                    userManager.AddToRoleAsync(user, "Member");
+                }
+
+                // Creade Admin User
+                var adminUser = new User
+                {
+                    UserName = "Admin"
+                };
+
+                var result = userManager.CreateAsync(adminUser, "!@#_Dating_App_Admin@2019").Result;
+
+                if (result.Succeeded)
+                {
+                    var admin = userManager.FindByNameAsync("Admin").Result;
+                    userManager.AddToRolesAsync(admin, new[] { "Admin", "Moderator" });
                 }
             }
         }
